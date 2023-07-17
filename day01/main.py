be96406fbdfbf46f38545e44d36ed8d1beb3321a
teacher <vincentjava@yahoo.com.tw>
2023-07-17 11:01:41 +0800

    python

---
+++ day01/main.py
@@ -0,0 +1,6 @@
+# -*- coding:UTF-8 -*-
+print("Hello Python")
+print("Hello Python")
+print("Hello Python")
+print("Hello Python")
+print("我是中文")
--- main.py
+++
@@ -1,4 +0,0 @@
-print("Hello Python")
-print("Hello Python")
-print("Hello Python")
-print("Hello Python")
