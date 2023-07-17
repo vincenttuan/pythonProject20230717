017e9ba9d817b4254ae95f05611dfc3b6d8f8450
teacher <vincentjava@yahoo.com.tw>
2023-07-17 12:04:17 +0800

    python

---
+++ day01/main6.py
@@ -0,0 +1,9 @@
+# 變數應用
+# 已知半徑 r = 9
+# 求"圓面積"與"球體積"各為多少 ?
+r = 9
+pi = 3.14
+area = pi * r ** 2   # 圓面積
+volume = 4/3 * pi * r ** 3  # 球體積
+print(area, volume)
+print('圓面積 %.2f 球體積  %.2f' % (area, volume))
