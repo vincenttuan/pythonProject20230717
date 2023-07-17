c5dc2495c14e1675543c0ef6abc232f0c2bf18b9
teacher <vincentjava@yahoo.com.tw>
2023-07-17 11:12:44 +0800

    python

---
+++ day01/main3.py
@@ -0,0 +1,10 @@
+# 變數的應用
+x = 100  # 國文成績
+y = 80   # 英文成績
+total = x + y  # 總分
+average = total / 2  # 平均
+print(x, y, total, average)
+print('國文', x)
+print('英文', y)
+print('總分', total)
+print('平均', average)
