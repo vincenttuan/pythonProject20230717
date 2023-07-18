b2eaf094350174e77c011fa6d319ff3f996dcbe5
teacher <vincentjava@yahoo.com.tw>
2023-07-18 11:16:04 +0800

    python

--- day02/main5.py
+++ day02/main5.py
@@ -6,4 +6,9 @@ print(num)
 
 # 樂透三星彩電腦選號
 # 0~9 取出任意三個數字
-
+n1 = random.randint(0, 9)
+n2 = random.randint(0, 9)
+n3 = random.randint(0, 9)
+print('電腦選號', n1, n2, n3)
+print('電腦選號 %d %d %d' % (n1, n2, n3))
+print('電腦選號 {0} {1} {2}'.format(n1, n2, n3))
