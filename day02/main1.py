ad30f14719f9922800ff1a43f3a30638e49a3a47
teacher <vincentjava@yahoo.com.tw>
2023-07-18 09:51:54 +0800

    python

--- day02/main1.py
+++ day02/main1.py
@@ -8,6 +8,12 @@ print(name1, name2, age, bmi)
 print("姓名", name1, "年齡", age, "BMI值", bmi)
 # 印出(格式化, %s 放字串, %d 放整數, %f 放符點數)
 print("姓名 %s 年齡 %d BMI值 %.2f" % (name1, age, bmi))
-# 印出(格式化 {}, {0}, {name})
+# 高級印出(格式化 {}, {0}, {name})
 print("姓名 {} 年齡 {} BMI值 {}".format(name1, age, bmi))
 print("姓名 {} 年齡 {} BMI值 {:.2f}".format(name1, age, bmi))
+print("姓名 {0} 年齡 {1} 姓名 {2} 年齡 {1}".format(name1, age, name2))
+
+web_name = "雅虎"
+web_url = "https://tw.yahoo.com"
+print("網站名 {name} 網址 {url}".format(name=web_name, url=web_url))
+
