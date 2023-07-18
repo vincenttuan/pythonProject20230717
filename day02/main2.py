277ce9fec8450bfa1b08b38530ec461ce9114bb4
teacher <vincentjava@yahoo.com.tw>
2023-07-18 10:06:31 +0800

    python

--- day02/main2.py
+++ day02/main2.py
@@ -3,4 +3,11 @@ chinese = input('請輸入國文成績:')
 print('國文成績是', chinese)
 print('國文成績是 %s' % chinese)
 print('國文成績是 {0}'.format(chinese))
+english = input('請輸入英文成績:')
+print('英文成績是', english)
+print('英文成績是 %s' % english)
+print('英文成績是 {0}'.format(english))
+# 計算總分 = ?
+print(chinese + english)
+print(int(chinese) + int(english))  # 利用 int() 將字串轉成整數
 
