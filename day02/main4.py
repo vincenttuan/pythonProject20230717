7b249a2d6b65bf0b5ea6f09c4ec14e749dbaf64f
teacher <vincentjava@yahoo.com.tw>
2023-07-18 11:05:27 +0800

    python

--- day02/main4.py
+++ day02/main4.py
@@ -3,8 +3,11 @@
 # 例如: 輸入 12 印出 偶數
 #      輸入 15 印出 奇數
 # 提示: 利用 % 餘數與 == 來進行判斷
-x = int(input('請輸入一個整數:'))
-if x % 2 == 0:
-    print("%d 是偶數" % x)
-else:
-    print("%d 是奇數" % x)
+try:
+    x = int(input('請輸入一個整數:'))
+    if x % 2 == 0:
+        print("%d 是偶數" % x)
+    else:
+        print("%d 是奇數" % x)
+except:
+    print('資料輸入錯誤')
