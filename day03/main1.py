3beac33ff66fd2314ac5dbae4446e3b9fc4a45c4
teacher <vincentjava@yahoo.com.tw>
2023-07-19 09:34:17 +0800

    python

--- day03/main1.py
+++ day03/main1.py
@@ -2,13 +2,19 @@
 data = "100,90,80,70,60"  # 學生成績資料
 scores = data.split(",")
 print(len(scores))
-print(scores[0])
-print(scores[1])
-print(scores[2])
-print(scores[3])
-print(scores[4])
+print(scores[0], type(scores[0]))
+print(scores[1], type(scores[1]))
+print(scores[2], type(scores[2]))
+print(scores[3], type(scores[3]))
+print(scores[4], type(scores[4]))
 # 計算總成績
-sum = scores[0] + scores[1] + scores[2] + scores[3] + scores[4]
-print(sum)
+total = scores[0] + scores[1] + scores[2] + scores[3] + scores[4]
+print("總成績:", total)
+# 改成每一個元素必須透過 int() 來轉型
+total = int(scores[0]) + int(scores[1]) + int(scores[2]) + int(scores[3]) + int(scores[4])
+print("總成績:", total)
+# 計算平均
+average = total / len(scores)  # total / 5
+print("平均:", average)
 
 
