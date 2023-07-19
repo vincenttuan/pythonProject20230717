3eceb1a78ad45f77305d0e65e0828646e73addc2
teacher <vincentjava@yahoo.com.tw>
2023-07-19 11:23:13 +0800

    python

--- day03/猜數字.py
+++ day03/猜數字.py
@@ -3,10 +3,13 @@
 # 若猜得比答案大 -> 顯示猜大了
 import random
 answer = random.randint(1, 9)
-print('answer =', answer)
-while True:
+count = 3
+#print('answer =', answer)
+while count > 0:
     # 請玩家猜
     user_guess = int(input('玩家 1~9 請猜一個數字:'))
+    # count 扣掉一次
+    count = count - 1
     # 比對答案
     if user_guess > answer:
         print('玩家猜大了')
