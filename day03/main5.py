9bf947d19a51cf6e84a5b307bb747275e235593d
teacher <vincentjava@yahoo.com.tw>
2023-07-19 10:31:59 +0800

    python

--- day03/main5.py
+++ day03/main5.py
@@ -1,7 +1,7 @@
 # 字串分析 - 切割(split)
 # 正則表達式
 import re
-data = "國=100,英=90,數=75.4"
+data = "國=100,英=90,數:75.4"
 # 解法: scores = [100.0, 90.0, 75.4]
 # 利用正則表達式找出所有的數字
 scores = [float(score) for score in re.findall(r'[\d\.]+', data)]
