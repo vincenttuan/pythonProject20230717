9d7a83d52dec18a0a6bda4895ca3b017872c5880
teacher <vincentjava@yahoo.com.tw>
2023-07-19 11:46:46 +0800

    python

--- day03/猜數字.py
+++ day03/猜數字.py
@@ -1,11 +1,11 @@
 # 1~9 猜數字
 # 若猜得比答案小 -> 顯示猜小了
 # 若猜得比答案大 -> 顯示猜大了
-# 若猜三次仍沒猜出來要顯示 GG
+# 若猜三次仍沒猜出來要顯示 "Sorry 次數已用完您沒猜對"
 import random
 answer = random.randint(1, 9)
 count = 3
-#print('answer =', answer)
+print('answer =', answer)
 while count > 0:
     # 請玩家猜
     user_guess = int(input('玩家 1~9 請猜一個數字:'))
@@ -19,7 +19,9 @@ while count > 0:
     else:
         print('玩家答對了')
         break
+
     # 判斷是否 GG
     if count == 0:
-        print("GG")
+        print("GG, Sorry 次數已用完您沒猜對")
 
+print("遊戲結束")
