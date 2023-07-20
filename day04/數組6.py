6835fc71ee1c39d3164c98d1ec1841a59adb0a3e
teacher <vincentjava@yahoo.com.tw>
2023-07-20 11:24:48 +0800

    python

--- day04/數組6.py
+++ day04/數組6.py
@@ -10,10 +10,18 @@ print(numbers)
 numbers2 = list(range(1, 50))
 print(numbers2)
 
+# 創建一個 0~9 的數字列表
+numbers3 = list(range(0, 10))
+print(numbers3)
 
-# 從數字列表中選擇 5 的不重複的數字
+# 從數字列表 numbers 中選擇 5 的不重複的數字
 lotto = r.sample(numbers, 5)
 print(lotto)
 
+# 從數字列表 numbers2 中選擇 6 的不重複的數字
 lotto2 = r.sample(numbers2, 6)
 print(lotto2)
+
+# 從數字列表 number3 中選擇 4 的可重複的數字
+lotto3 = r.choices(numbers3, k=4)  # choices() 可以重複
+print(lotto3)
