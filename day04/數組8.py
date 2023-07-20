00d5758fd384167e9e00f065bb8883b43cbd6710
teacher <vincentjava@yahoo.com.tw>
2023-07-20 12:01:52 +0800

    python

---
+++ day04/數組8.py
@@ -0,0 +1,21 @@
+# 數組 dict 的應用
+emp1 = {'name': 'John', 'salary': 60000}
+emp2 = {'name': 'Mary', 'salary': 50000}
+emp3 = {'name': 'Jack', 'salary': 80000}
+emp4 = {'name': 'Rose', 'salary': 70000}
+# 請問總薪資為何 ?
+employees = [emp1, emp2, emp3, emp4]
+print(employees)
+# 使用 for-in + sum() 來計算
+total_salary = sum(emp['salary'] for emp in employees)
+print(total_salary)
+# 請問誰(name)的薪資最高 ?
+highest_salary = 0
+highest_name = ''
+# 透過替換法
+for emp in employees:  # 輪詢每一個員工
+    if emp['salary'] > highest_salary:
+        highest_salary = emp['salary']
+        highest_name = emp['name']
+
+print(highest_name, highest_salary)
