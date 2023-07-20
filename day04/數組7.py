67288a6484e99d06ad59c0dcfc6d64a0c3164b66
teacher <vincentjava@yahoo.com.tw>
2023-07-20 11:40:42 +0800

    python

---
+++ day04/數組7.py
@@ -0,0 +1,16 @@
+# 數組 dict(字典格式), 每一組元素不可重複
+# 元素 key:value (key 不可重複, value 可重複)
+student1 = {'name': 'John', 'score': 95, 'age': 17, 'birth': '2006-7-20'}
+print(student1)
+print(student1['name'])
+print(student1['score'])
+# 加入性別
+student1['sex'] = 'M'
+print(student1)
+# 加入分數 (若元素的 key 相同, 則原 value 會被取代)
+student1['score'] = 100
+print(student1)
+# 刪除元素 'birth'
+del student1['birth']
+print(student1)
+
