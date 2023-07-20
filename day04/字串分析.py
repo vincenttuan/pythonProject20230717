b5ce86d72fc6a134c4798d78896d563e9fd468fe
teacher <vincentjava@yahoo.com.tw>
2023-07-20 09:32:17 +0800

    python

--- day04/字串分析.py
+++ day04/字串分析.py
@@ -5,6 +5,15 @@ print(data.find('$') + 1)  # 5
 print(data.find(','))  # 10
 print(data[5:10])
 print(data[data.find('$') + 1:data.find(',')])
-
+print(data[data.find('發放') + 2:data.find('個月')])
+# --------------------------------------------------
+salary = int(data[data.find('$') + 1:data.find(',')])
+bonus_months = int(data[data.find('發放') + 2:data.find('個月')])
+# 計算年終
+year_end_bonus = salary * bonus_months
+print('小明的年終獎金是 :', year_end_bonus)
+print('小明的年終獎金是 : %d' % year_end_bonus)
+print('小明的年終獎金是 : {}'.format(year_end_bonus))
+print(f'小明的年終獎金是 : {year_end_bonus}')
 
 
