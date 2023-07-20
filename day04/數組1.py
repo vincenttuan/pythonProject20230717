7eae27c24137d984b666d9219ffa9816255f8a05
teacher <vincentjava@yahoo.com.tw>
2023-07-20 10:13:55 +0800

    python

---
+++ day04/數組1.py
@@ -0,0 +1,14 @@
+# 數組:
+# []     -> 稱為 List/陣列, 資料元素可重複
+# set()  -> 稱為 Set, 資料元素不會重複
+# dict() -> 稱為字典格式/map, 資料(key/value的形式) key 不可重複, value 可重複
+a = [1, 3, 5, 7, 5]
+print(a)
+a[0] = 2  # 變更元素內容
+print(a)
+a.append(9)  # 增加元素內容
+print(a)
+a.remove(5)  # 移除元素(5是指元素的內容)
+print(a)
+a.__delitem__(2)  # 移除元素(2是指元素的位置)
+print(a)
