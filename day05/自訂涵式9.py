7848812c74bc38aa2adcfb5e4f0d3d67a24de720
teacher <vincentjava@yahoo.com.tw>
2023-07-21 12:02:42 +0800

    python

---
+++ day05/自訂涵式9.py
@@ -0,0 +1,19 @@
+# 自訂函數
+# 我可以放任意的參數數量
+# get_sum(10, 20), get_sum(10, 20, 30), get_sum(10, 20, 30, 40)
+def get_sum(*num):
+    return sum(num)
+
+
+def fun(**kwargs):  # ** 支援字典格式
+    for key, vale in kwargs.items():
+        print(key, vale)
+
+if __name__ == '__main__':
+    print(get_sum())
+    print(get_sum(10, 20))
+    print(get_sum(10, 20, 30, 40, 50))
+
+    fun(a=4, b=5)
+    fun(score=100, height=170, weight=60)
+
