5462068fc205e0cd7377495d57c3efc4336353aa
teacher <vincentjava@yahoo.com.tw>
2023-07-21 09:20:33 +0800

    python

---
+++ day05/迴圈For.py
@@ -0,0 +1,15 @@
+# 迴圈 for
+import random as r
+# for loop 步進迴圈
+for i in range(1, 5):  # 1, 2, 3, 4
+    print(i, end=" ")
+#-----------------------------------
+print()  # 換行
+for i in range(1, 5):
+    print("Python", end=" ")
+#-----------------------------------
+print()  # 換行
+for i in range(1, 5):
+    print(i, "Python", end=" ")
+#-----------------------------------
+
