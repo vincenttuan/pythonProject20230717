949cd1bd8e932e0ae5303390fdfb0a73d282bab5
teacher <vincentjava@yahoo.com.tw>
2023-07-21 09:45:29 +0800

    python

---
+++ day05/自訂涵式2.py
@@ -0,0 +1,14 @@
+# def 自訂涵式
+import random as r
+
+
+def print_luck_number():
+    print(r.randint(1, 999))
+
+
+# 主程式區塊
+if __name__ == '__main__':
+    print_luck_number()
+    print_luck_number()
+
+
