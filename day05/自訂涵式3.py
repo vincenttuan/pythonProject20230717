ee4d154fb60c44c9939748b8e197d4caad9d7a75
teacher <vincentjava@yahoo.com.tw>
2023-07-21 09:51:12 +0800

    python

---
+++ day05/自訂涵式3.py
@@ -0,0 +1,12 @@
+# 自訂涵式
+
+def add(x, y):  # add 自訂涵式
+    total = x + y
+    print('Total:', total)
+
+
+if __name__ == '__main__':  # 主程式(由此開始執行)
+    add(10, 20)  # 調用 add 自訂涵式
+    add(20, 30)
+
+
