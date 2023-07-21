44c4fd9c9a25e2045209493615320f14fcf20b4b
teacher <vincentjava@yahoo.com.tw>
2023-07-21 09:59:26 +0800

    python

--- day05/自訂涵式4.py
+++ day05/自訂涵式4.py
@@ -5,8 +5,16 @@ def div(x, y):
     result = x / y
     return result
 
+def mult(x, y):
+    result = x * y
+    return result
 
 if __name__ == '__main__':
     result = div(123, 19)
     print("result =", result)
     print("result = %.2f" % result)
+
+    result2 = mult(55, 13.7)
+    print("result2 =", result2)
+    print("result2 = %.2f" % result2)
+
