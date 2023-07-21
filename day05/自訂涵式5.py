89bf5a72c5c7628d78fce302bf50f062556aad13
teacher <vincentjava@yahoo.com.tw>
2023-07-21 10:12:11 +0800

    python

---
+++ day05/自訂涵式5.py
@@ -0,0 +1,11 @@
+# 自訂涵式 return 多組資料
+# 同時計算乘與除的結果
+def multi_and_div(x, y):
+    multi_result = x * y  # 相乘的結果
+    div_result = x / y    # 相除的結果
+    return multi_result, div_result  # 同時回傳二個結果
+
+
+
+
+
