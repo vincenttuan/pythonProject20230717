4624324987e69e01041d6af7e6037edc24642b8b
teacher <vincentjava@yahoo.com.tw>
2023-07-24 11:45:35 +0800

    python

--- day06/寫入檔案3.py
+++ day06/寫入檔案3.py
@@ -11,3 +11,10 @@ print("更新前:", old_salaries)
 old_salaries.update(new_salaries)
 print("更新後:", old_salaries)
 
+# 將更新後的內容寫回到原始文件
+# map 轉換, 將字典格式 {name, salary} 轉成字串 "name salary"
+# lambda x, lambda 是一個涵式, x 指個是 old_salaries.items() 中的每一項
+with open('salary.txt', 'w', encoding='UTF-8') as file:
+    file.write('\n'.join(map(lambda x: f'{x[0]} {x[1]}', old_salaries.items())) + '\n')
+
+print('寫入 OK')
