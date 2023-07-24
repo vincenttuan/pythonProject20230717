90a29ff6e1d3ac8eb75a1a3c1e5347346d6fcc39
teacher <vincentjava@yahoo.com.tw>
2023-07-24 11:08:23 +0800

    python

--- day06/寫入檔案.py
+++ day06/寫入檔案.py
@@ -1,7 +1,8 @@
 salaries = [("Rose", 38000), ("Helen", 77000), ("Jo", 56000)]
 
 # 開啟 salaries.txt 文件並寫入資料
-with open('salary.txt', 'w', encoding='UTF-8') as file:
+# w: 完整寫入(不保留原來的資料), a: append 加入(保留原來的資料)
+with open('salary.txt', 'a', encoding='UTF-8') as file:
     for name, salary in salaries:
         # format 標準版
         # file.write("{} {}\n".format(name, salary))
