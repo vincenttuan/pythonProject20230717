3f6f0467987e20f5b4cdc53b9f20413ceaedbf7d
teacher <vincentjava@yahoo.com.tw>
2023-07-24 10:26:03 +0800

    python

--- day06/讀取檔案4.py
+++ day06/讀取檔案4.py
@@ -11,7 +11,12 @@ matches = pattern.findall(content)
 salaries = [int(salary) for name, salary in matches]
 print(salaries)
 
-
-
 # 計算總薪資, 平均薪資, 最高薪資, 最低薪資
+total_salary = sum(salaries)
+total_person = len(salaries)
+average_salary = total_salary / total_person
+max_salary = max(salaries)
+min_salary = min(salaries)
 
+print('資料筆數:{}\n總薪資:{}\n平均薪資:{:.1f}\n最高薪資:{}\n最低薪資:{}'
+      .format(total_person, total_salary, average_salary, max_salary, min_salary))
