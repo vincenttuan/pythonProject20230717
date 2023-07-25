5a1ab401ae274e595f5429ed9284601b7de4f6d9
teacher <vincentjava@yahoo.com.tw>
2023-07-25 10:03:42 +0800

    python

--- day07/基礎統計分析與繪圖.py
+++ day07/基礎統計分析與繪圖.py
@@ -1,6 +1,7 @@
 # 比較 salary 與 age 哪一個分散程度大(集中度小)?
 from day07.基礎統計分析 import get_from_file
 import statistics as stat
+import matplotlib.pyplot as plt
 
 # 計算並取得變異係數
 def get_cv(values):
@@ -21,3 +22,14 @@ if __name__ == '__main__':
     print(ages, 'cv =', ages_cv)
 
     print("{} 的分散程度大".format('salary' if salaries_cv > ages_cv else 'age'))
+
+    # 繪製統計圖表
+    all_salaries = salaries.values()
+    all_ages = ages.values()
+    all_names = ages.keys()
+    print(all_salaries, all_ages, all_names)
+
+    plt.plot(all_names, all_salaries)
+    plt.plot(all_names, all_ages)
+
+    plt.show()
