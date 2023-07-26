128bd170b8827ff08b951a32c22141d9bf5ceb62
teacher <vincentjava@yahoo.com.tw>
2023-07-26 11:16:56 +0800

    python

--- day08/分析股票資訊-pandas.py
+++ day08/分析股票資訊-pandas.py
@@ -1,8 +1,22 @@
+import io
 import pandas as pd
+import datetime
+import requests
 
 # 讀取本地的 CSV 檔
-data = pd.read_csv('BWIBBU_d_ALL_20230725_utf8.csv', header=1, encoding='utf-8')
+# data = pd.read_csv('BWIBBU_d_ALL_20230725_utf8.csv', header=1, encoding='utf-8')
 
+# 讀取雲端資料
+date = datetime.datetime(2023, 7, 25)
+date_str = date.strftime('%Y%m%d')
+url = f'https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date={date_str}&selectType=ALL'
+# 透過 requests 取雲端資料, 解決 SSL 簽證問題
+response = requests.get(url)
+data_io = io.StringIO(response.text)  # 類似一個檔案, 只是該檔案在記憶體
+
+data = pd.read_csv(data_io, header=1, encoding='utf-8')
+
+#----------------------------------------------------------------------------------
 # 列印 DataFrame 的欄位名稱
 #print(data.columns)
 
