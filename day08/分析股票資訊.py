8b8a89e713b2f14b310db49eee5c91b779cdb5ce
teacher <vincentjava@yahoo.com.tw>
2023-07-26 09:48:51 +0800

    python

--- day08/分析股票資訊.py
+++ day08/分析股票資訊.py
@@ -12,5 +12,18 @@ if __name__ == '__main__':
     url = 'https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date={}&selectType=ALL'
     url = url.format(date_str)
     print(url)
-
+    # 抓取網頁資料
+    data = requests.get(url).text
+    # 統一去除雙引號 "
+    data = data.replace('"', '')
+    # 將 - 換成 -1
+    data = data.replace('-', '-1')
+    #print(data)
+    # 發現若以 split(',') 來切割每一筆紀錄, 有用的欄位個數會是 8
+    twii = []  # 用來放入有用的數組
+    for row in data.split('\r\n'):  # windows 的換行 '\r\n', mac, linux 的換行 '\n'
+        row = row.split(',')
+        #print(len(row))
+        if len(row) == 8:
+            print(row)
 
