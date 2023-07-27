48c5b1a8c7ded7a1eb42afbedfeda67ee989b85d
teacher <vincentjava@yahoo.com.tw>
2023-07-27 09:34:27 +0800

    python

--- day09/GUI視窗1.py
+++ day09/GUI視窗1.py
@@ -2,13 +2,15 @@ import tkinter
 
 win = tkinter.Tk()
 
-label = tkinter.Label(win, text="Hello")
-label.pack()
+win.title("我的 GUI 視窗")
+
+label = tkinter.Label(win, text="Hello", bg='red', fg='yellow', font=('Arial, 18'), width=30, height=3)
+label.pack(padx=10, pady=10)
 
 button_ok = tkinter.Button(win, text="OK")
-button_ok.pack(side=tkinter.LEFT)
+button_ok.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=True, padx=10, pady=10)
 
 button_cancel = tkinter.Button(win, text="Cancel")
-button_cancel.pack(side=tkinter.RIGHT)
+button_cancel.pack(side=tkinter.RIGHT, fill=tkinter.BOTH, expand=True, padx=10, pady=10)
 
 win.mainloop()
